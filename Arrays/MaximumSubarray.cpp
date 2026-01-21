9b839bc448102bfa0fe514a4fcef774ab523160e
Bhrugu0516 <bhruguchaudhari1605@gmail.com>
2026-01-21 20:51:51 +0530

    Day 3: Maximum Subarray

---
+++ Arrays/MaximumSubarray.cpp
@@ -0,0 +1,31 @@
+/*
+Problem: Maximum Subarray
+Link: https://leetcode.com/problems/maximum-subarray/
+Topic: Arrays
+
+Approach:
+- Use Kadane’s Algorithm
+- Maintain current subarray sum and maximum sum
+- Reset current sum when it becomes smaller than current element
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:
+       int MaximumSub(vector<int>& nums){
+        int maxSum=nums[0];
+        int currSum=nums[0];
+        for(int i=0;i<nums.size();i++){
+            currSum=max(currSum[i]+nums[i],nums[i]);
+            maxSum=max(maxSum,currSum);
+
+        }
+        return maxSum;
+       }
+}
