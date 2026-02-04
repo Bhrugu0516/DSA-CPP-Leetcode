3c2e7694b2cfc09c1e4c6eef8ac69b90646a38ea
Bhrugu0516 <bhruguchaudhari1605@gmail.com>
2026-02-04 18:54:04 +0530

    Day 17: Implement Stack using Queues

---
+++ Stack/ImplementStackUsingQueues.cpp
@@ -0,0 +1,52 @@
+/*
+Day 17 - Implement Stack using Queues
+
+Idea:
+- Use one queue
+- Rotate elements after each push to simulate LIFO
+
+Time Complexity:
+- push: O(n)
+- pop/top: O(1)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+class MyStack {
+public:
+queue<int> q;
+
+    MyStack() {}
+    
+    void push(int x) {
+        int size=q.size();
+        q.push(x);
+        for(int i=0;i<size;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int top=q.front();
+        q.pop();
+        return top;
+        
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
