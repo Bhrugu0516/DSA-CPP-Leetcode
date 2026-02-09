af549a09164d1ecd51f84482a0920224fdccf817
Bhrugu0516 <bhruguchaudhari1605@gmail.com>
2026-02-09 19:31:02 +0530

    Day 21: Max Consecutive Ones III

---
+++ SlidingWindow/MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,24 @@
+/*
+Day 21 - Max Consecutive Ones III
+Topic: Sliding Window (At most K zeros)
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+    int left=0,zeroCount=0,maxLen=0;
+    for(int right=0;right<nums.size();right++){
+        if(nums[right]==0)zeroCount++;
+        while(zeroCount>k){
+            if(nums[left]==0)zeroCount--;
+            left++;
+        }
+        maxLen=max(maxLen,right-left+1);
+    }
+    return maxLen;
+    }
+};
