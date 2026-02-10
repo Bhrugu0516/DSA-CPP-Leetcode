2de250db248ee0dca6492f079821a856dc82bf1c
Bhrugu0516 <bhruguchaudhari1605@gmail.com>
2026-02-10 19:27:40 +0530

    Day 22: Binary Search (classic)

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,30 @@
+/*
+Day 22 - Binary Search
+Topic: Binary Search (Classic)
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
